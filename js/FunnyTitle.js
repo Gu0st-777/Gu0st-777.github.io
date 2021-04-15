37753bc573bb3ebeb1c41e7ae59c732c4a0b3275
Gu0st-777 <18439225744@163.com>
2021-04-15 13:27:04 +0800

    Site updated: 2021-04-15 13:27:04

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ这就要抛弃我了吗？';
+         document.title = '这就要抛弃我了吗？';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ小哥哥，小姐姐快来玩啊！' + OriginTitle;
+         document.title = '小哥哥，小姐姐快来玩啊！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
